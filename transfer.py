0e181b897e6cea1ad797ca0a4a0470789466706f
9bie <blackguwc@163.com>
2019-02-27 17:19:37 +0800

    看来真的得需要一个游标卡尺了

--- transfer.py
+++ transfer.py
@@ -15,7 +15,7 @@ class tcp2tcp(object):
         :param remote_port: 远程主机端口
         :param reconn: 最大重连次数，设置为0则一直重连，每次间隔3s,-1则为不重连
         '''
-        self.s_client = socket.socket()
+
         self.server = socket.socket()
         self.server.bind(("0.0.0.0", local_port))
         self.server.listen(10)
@@ -38,7 +38,7 @@ class tcp2tcp(object):
         '''
         if not arg2:
             for i in self.struct:
-                if arg2 in i[0] or arg2 in i[1]:
+                if arg1 in i:
                     return i
         else:
             package = [arg1,arg2,True]
@@ -49,26 +49,35 @@ class tcp2tcp(object):
 
     def __connect(self,remote_host,remote_port):
         try:
-            socket =self.s_client.connect((remote_host, remote_port))
-            return socket
+            s_client = socket.socket()
+            s_client.connect((remote_host, remote_port))
+            print("连接成功")
+            return s_client
         except:
             print("连接失败")
-            return ""
+            return None
     def __disconnect(self,package):
+        print("用户退出")
         package[0].close()
         package[1].close()
         del self.queue[package[0]]
         del self.queue[package[1]]
-        self.inputs.remove(package[0])
-        self.inputs.remove(package[1])
+        if package[0] in self.inputs:
+            self.inputs.remove(package[0])
+        elif package[1] in self.inputs:
+            self.inputs.remove(package[1])
+        elif package[0] in self.outputs:
+            self.outputs.remove(package[0])
+        elif package[1] in self.outputs:
+            self.outputs.remove(package[1])
         self.struct.remove(package)
     def __handle(self):
         self.inputs.append( self.server)
 
         while True:
-            if self.isloop:
-                print("线程正在运行中。")
-                return None
+            if self.flag:
+                return
+            print(self.inputs,self.outputs)
             r,w,e = select.select(self.inputs,self.outputs,self.inputs)
             # 读线程
             for s in r:
@@ -81,8 +90,7 @@ class tcp2tcp(object):
                     while True:
                         i += 1
                         cli = self.__connect(self.remote_host, self.remote_port)
-                        if cli is not None:
-                            print("连接成功")
+                        if cli:
                             break
                         elif i == self.reconn and self.reconn != 0 or self.reconn == -1:
                             print("到达最大重连次数还未成功。断开连接.")
@@ -96,17 +104,22 @@ class tcp2tcp(object):
                         # 远端套接字也要作为收管道进行handle
                         self.inputs.append(cli)
                         self.__set_get(cli,conn)
-                    else:
-                        # 说明是老用户进入
+                else:
+                    # 说明是老用户进入
+                    package = self.__set_get(s)
+                    try:
                         recv = s.recv(20480)
-                        package = self.__set_get(s)
-                        if not recv:
-                            # 没消息了
-                            self.__disconnect(package)
-                        else:
-                            self.queue[s].put(recv)
-                            if s not in self.outputs:
-                                self.outputs.append(s)
+                    except:
+                        self.__disconnect(package)
+
+                    if not recv:
+                        # 没消息了
+                        self.__disconnect(package)
+
+                    else:
+                        self.queue[s].put(recv)
+                        if s not in self.outputs:
+                            self.outputs.append(s)
             # 写线程
             for s in w:
                 package = self.__set_get(s)
@@ -119,13 +132,21 @@ class tcp2tcp(object):
                         package[1].sendall(send_data)
                     else:
                         package[0].sendall(send_data)
+                    self.outputs.remove(s)
             for s in e:
+                print("有错误。")
                 package=self.__set_get(s)
                 self.__disconnect(package)
 
-    def start(self):
-
+    def start(self,thread=False):
+        if self.isloop:
+            print("线程正在运行中。")
+            return None
         self.isloop = True
+        if thread:
+            threading.Thread(target=self.__handle,args=()).start()
+        else:
+            self.__handle()
 
 
 if __name__ =="__main__":
