3e78aa691e78fd7280e8f706733a19e834e22eb6
bakabie <blackguwc@163.com>
2019-03-20 01:17:00 +0800

    添加udp 53透明代理

--- transfer.py
+++ transfer.py
@@ -324,17 +324,133 @@ class socks2http(object):
         pass
     def start(self,thread=False):
         pass
+class httproxy2udp(object):
+    def __init__(self,remote_server,port,timeout=60):
+        self.remote_server = remote_server
+        self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.fake_dns = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.fake_dns.bind(("0.0.0.0",53))
+        self.s = socket.socket()
+        self.s.bind(("0.0.0.0",port))
+        self.s.listen(10)
+        self.timeout = timeout
+        # self.stack_index = {}# 0 没读 1 读了
+        self.data_stacks = {}
+        self.BUFLEN = 8192
+        self.VERSION = 'Python Proxy on dns 53 udp version/0.1.1 modified by bakabie'
+        self.HTTPVER = 'HTTP/1.1'
+        threading.Thread(target=self._sweeper_).start()
+        threading.Thread(target=self.fake_dns).start()
+    def __get_base_header(self,client):
+        client_buffer=""
+        while 1:
+            client_buffer += client.recv(self.BUFLEN).decode()
+            end = client_buffer.find('\n')
+            if end != -1:
+                break
+        print('%s' % client_buffer[:end])  # debug
+        data = (client_buffer[:end + 1]).split()
+        client_buffer = client_buffer[end + 1:]
+        data.append(client_buffer)
+
+        return data
+    def _sweeper_(self):
+        while True:
+            for i in self.data_stacks.keys():
+                if int(time.time())-int(i)>=self.timeout
+            time.sleep(self.timeout)
+    def _fake_dns_server(self):
+        while True:
+            data, addr = s.recvfrom(20480)
+            if not data:
+                continue
+
+            data=data.encode()
+            packet = data[data.find("!#strt#!"):data.find("~#end#~")+7]# 愚蠢而又低效率的方案
+            if not packet:
+                continue
+            timestamp = packet[packet.find("!#strt#!")-8:packet.find("!timestamp$")]
+            if not timestamp:
+                continue
+            data = packet[packet.find("$trued#!")-8:packet.find("~#end#~")]
+            if int(timestamp)-int(time.time()) > self.timeout:
+                continue
+                # 超时丢弃
+            if timestamp not in self.data_stacks:
+                self.data_stacks[timestamp] = data
+                
+
+    def _read_write(self,client,target):
+        time_out_max = self.timeout / 3
+        socs = [client, target]
+        count = 0
+        while 1:
+            count += 1
+            (recv, _, error) = select.select(socs, [], socs, 3)
+            if error:
+                break
+            if recv:
+                for in_ in recv:
+                    data = in_.recv(self.BUFLEN)
+                    if in_ is client:
+                        out = target
+                    else:
+                        out = client
+                    if data:
+                        out.send(data)
+                        count = 0
+            if count == time_out_max:
+                break
+    def _get_target(self, host):
+        i = host.find(':')
+        if i != -1:
+            port = int(host[i + 1:])
+            host = host[:i]
+        else:
+            port = 80
+        return (host,port)
+    def __handle(self,client,address):
+        method,path,protocol,client_buffer =self.__get_base_header(client)
+        if method == 'CONNECT':
+            target = self._get_target(path)
+            client.send((self.HTTPVER + ' 200 Connection established\n' +
+                        'Proxy-agent: %s\n\n' % self.VERSION).encode())
+            self._read_write(client=client,target=target)
+            client.close()
+            
+        elif method in ('OPTIONS', 'GET', 'HEAD', 'POST', 'PUT',
+                             'DELETE', 'TRACE'):
+            path = path[7:]
+            i = path.find('/')
+            host = path[:i]
+            path2 = path[i:]
+            target = self._connect_target(host)
+            target.send(('%s %s %s\n' % (method, path2, protocol) +
+                        client_buffer).encode())
+            self._read_write(client=client,target=target)
+            client.close()
+            target.close()
+
+    def __real_start(self):
+        while True:
+                conn,address = self.s.accept()
+                threading.Thread(target=self.__handle,args=(conn,address)).start()
+    def start(self,thread=False):
+        if thread:
+            threading.Thread(target=self.__real_start)
+        else:
+            self.__real_start()
 if __name__ =="__main__":
     text="""
     ########################MY HOLE#########################
     # WHAT 'S THIS?  JUST A SOCKETS/WEB TOOLKIT FOR MYSELF #
     # HOW TO USE IT?                                       #
-    # <MHOLE.PY> -M [MOD] -B [LOCAL PORT] \                #
+    # <MHOLE.PY> -M [MOD] -B [LOCAL PORT]                  #
     #           -R [REMOTEADDRESS:PORT] -N [RECONNECT NUM] #
     # YOU CAN USE THESE MOD:                               #
-    # TCP2TCP:  A SOCKET TRANSFER TOOLKIE. SET LOCAL PORT\ #
-    #         AND REMOTE PORT ABD RECONNECT NUMBER. ANY  \ #
-    #         DATE PASS HERE WILL TRANSFER TO OTHER WAT. \ #
+    # TCP2TCP:  A SOCKET TRANSFER TOOLKIE. SET LOCAL PORT  #
+    #         AND REMOTE PORT ABD RECONNECT NUMBER. ANY    #
+    #         DATE PASS HERE WILL TRANSFER TO OTHER WAT.   #
     #        OF COUSE,THIS CAN USE TO  SOLE PLCAING AGENCY #
     # HTTPROXY: A SIMPLE HTTP PROXY FROM <PYTHONPROXY.PY>  #
     ########################################################
